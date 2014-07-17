168dafea42df415bf97bf9e5edc14c5333b6550d
Paul Tuckey <paul@tuckey.org>
2014-07-17 13:57:45 +1200

    Updates to support windows client

--- javascript-api/quotes/libs/js/showcase-data.js
+++ javascript-api/quotes/libs/js/showcase-data.js
@@ -1,7 +1,7 @@
 /**
  * Showcase Data API
  *
- * Copyright 2013 Showcase Software Limited
+ * Copyright 2014 Showcase Software Limited
  */
 function SHOWCASE_DATA(settings) {
     if ( typeof settings != 'object' ) settings = {};
@@ -9,32 +9,22 @@ function SHOWCASE_DATA(settings) {
     var testMode = settings['testMode'] || false;
 
     var sc_call = function(type, key, value) {
-        var srcValue = key + ":##SC" + type + "##" + value;
-        var iframe = document.createElement("IFRAME");
-        iframe.setAttribute("src", srcValue);
-        document.documentElement.appendChild(iframe);
-        iframe.parentNode.removeChild(iframe);
-        iframe = null;
+        var is_windows_webview = /MSAppHost/i.test(navigator.userAgent);
+        var is_ios_uiwebview = /AppleWebKit/i.test(navigator.userAgent);
+        if ( is_windows_webview ) {
+            SHOWCASE_DATA_WIN_BRIDGE_PUSH({type: type, key: key, value: value});
+        }
+        if (is_ios_uiwebview) {
+            var srcValue = key + ":##SC" + type + "##" + value;
+            var iframe = document.createElement("IFRAME");
+            iframe.setAttribute("src", srcValue);
+            document.documentElement.appendChild(iframe);
+            iframe.parentNode.removeChild(iframe);
+            iframe = null;
+        }
     };
     if ( testMode ) {
-        var data = [{
-            name: "Quote 1",
-            saved: true,
-        },
-        {
-            name: "Quote 2",
-            saved: true,
-        },
-        {
-            name: "Quote 3",
-            saved: true,
-        },
-        {
-            name: "Quote 4",
-            saved: true,
-        }];
-        var jsonData = JSON.stringify(data);
-        var testData = {"current-quotes": jsonData};
+        var testData = {};
         sc_call = function(type, key, value) {
             if ( type == 'PUT' ) {
                 testData[key] = value;
@@ -67,4 +57,12 @@ function SHOWCASE_DATA(settings) {
 }
 var SHOWCASE_DATA_GLOBAL_GET_CALLBACK = function(key, value) {  // callback registered at the global level
     // nothing by default
+};
+
+var SHOWCASE_DATA_WIN_BRIDGE = [];
+var SHOWCASE_DATA_WIN_BRIDGE_PUSH = function(data) {
+    SHOWCASE_DATA_WIN_BRIDGE.push(JSON.stringify(data));
+};
+var SHOWCASE_DATA_WIN_BRIDGE_POP = function() {
+    return SHOWCASE_DATA_WIN_BRIDGE.pop();
 };
